01c55cb7dcdf5517c0c01be2b305ddc871045020
DrMetr <metronomedragon@gmail.com>
2025-04-07 17:19:41 +0300

    ...

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+            PaleBlue: 'hsl(193, 38%, 86%)',
+            Mint: 'hsl(150, 100%, 66%)',
+            GrayBlue: 'hsl(217, 19%, 38%)',
+            DarkBlue: 'hsl(217, 19%, 24%)',
+            BlackBlue: 'hsl(218, 23%, 16%)'
+        },
+        fontFamily: {
+            Manrop: ["Manrop", "sans-serif"],
+        },
+      },
+      plugins: [],
+    },
+  };
